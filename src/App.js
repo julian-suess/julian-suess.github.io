8aa1d7f86721d535b39a04551a09b135cbc14463
Julian Süss <c.suess@deersoft.de>
2018-07-17 13:51:09 +0200

    travis

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>Never Gonna Give You Up</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
